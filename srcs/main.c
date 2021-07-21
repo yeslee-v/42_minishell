c025b531e6cb093e51584807dfc31577fe98feb8
jaekpark <jaekpark@student.42seoul.kr>
2021-07-21 21:01:25 +0900

    [Write] a function to delete ENV node

--- srcs/main.c
+++ srcs/main.c
@@ -57,7 +57,13 @@ int		main(int ac, char **av, char **envp)
 	printf("before env change key = %s, value = %s\n", tmp->key, tmp->value);
 	tmp = change_env_value("USER", "MR. GOOD", g_sh.env);
 	printf("after env change key = %s, value = %s\n", tmp->key, tmp->value);
-	print_test_redir_fd();
+	printf("before delete\n");
+	print_env(g_sh.env);
+	printf("-----------------------\n");
+	printf(BLUE"after delete USER env node\n");
+	delete_env_node("USER", g_sh.env);
+	print_env(g_sh.env);
+	printf(RESET);
 	set_terminal();
 	while (ret)
 	{
